9d777208bd29b0a2a62bd10ec95701ca21c7e96f
Diego Claro de Moraes <39975253+di-claro-batera@users.noreply.github.com>
2025-03-12 19:01:16 -0300

    Update api.js
    teste para ver se roda no ec2

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'http://54.221.98.15:8000', // Use o IP da sua instância EC2
 });
 
 export default api;
