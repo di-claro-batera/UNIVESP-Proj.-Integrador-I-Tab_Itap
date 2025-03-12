abbf2c6c769baa5d4f3467f80b5311dc40703237
Diego Claro de Moraes <39975253+di-claro-batera@users.noreply.github.com>
2025-03-12 19:27:07 -0300

    teste ec2 amazon

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://54.221.98.15:8000', // Use o IP da sua instância EC2
+    baseURL: 'http://34.204.0.12:8000', // Use o novo IP da instância EC2
 });
 
 export default api;
